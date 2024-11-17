bcaa28f3409b7f2b297e9c815bd5ddc79c89cbe1
HP <username:ravihareesh1997@gmail.com>
2024-11-17 22:48:27 +0530

    TL CREATED methid1

--- GITMerging01/src/test/java/com/nt/upi/UpiPayment.java
+++ GITMerging01/src/test/java/com/nt/upi/UpiPayment.java
@@ -4,4 +4,8 @@ public class UpiPayment {
 public UpiPayment() {
 	System.out.println("first commited by DEVELOPER this class");
 }
+
+public void UpiPayments1() {
+	System.out.println("Tl write this method");
+}
 }
